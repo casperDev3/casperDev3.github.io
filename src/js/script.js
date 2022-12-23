c372c98f34ff1e664d8cd175f782072c61dec472
zerodott <93039953+casperDev3@users.noreply.github.com>
2022-12-23 16:07:44 +0200

    add black friday temp

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+let slider = $(document).ready(function () {
+  $('.testimonials__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    infinite: true
+  });
+});
+
+$('.header__burger').click(function(){
+  $('.header__burger').toggleClass('active');
+  $('.header__mobile-menu').toggleClass('active');
+})
+
